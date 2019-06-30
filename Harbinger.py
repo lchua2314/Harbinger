a482af10eb6ca2f6c114b6be694b703230191a7b
lchua2314 <46981223+lchua2314@users.noreply.github.com>
2019-06-29 18:09:01 -0700

    Made the animation smoother and made code easier to read.

--- Harbinger.py
+++ Harbinger.py
@@ -15,56 +15,47 @@ class MainFrame (Frame):
         self.myCanvas.grid() #Display it
 
         #Version
-        self.myCanvas.create_text(90, 10, text="Harbinger - Alpha 1.0.1", fill="black", font=("Fixedsys", 5))
+        self.myCanvas.create_text(90, 10, text="Harbinger - Alpha 1.1", fill="black", font=("Fixedsys", 5))
         
         #Creating the model of player 1 w/ label above it's head
         #Character is walking to the right of the screen
-        self.myCanvas.create_text(75, 100, text="Player 1 Model", width=1000, fill="black", anchor="nw", justify="center", font=("Fixedsys", 16)) #Name of character
+        #Name of the character
         a_name = self.myCanvas.create_text(75, 100, text="Player 1 Model", width=1000, fill="black", anchor="nw", justify="center", font=("Fixedsys", 16))
 
         #Creates the head of character
-        self.myCanvas.create_oval(125, 125, 150, 150, fill="white") #Head
         a_head = self.myCanvas.create_oval(125, 125, 150, 150, fill="white")
         
         #Torso
-        self.myCanvas.create_rectangle(130, 150, 145, 175, fill="white") #Torso
         a_torso = self.myCanvas.create_rectangle(130, 150, 145, 175, fill="white")
 
-        #HIS left arm
-        self.myCanvas.create_oval(115, 150, 130, 165, fill="white") #left arm
-        a_Larm = self.myCanvas.create_oval(115, 150, 130, 165, fill="white")
+        #Moved Left arm to bottom bc it is needed for the animations
 
-        #HIS right arm
-        self.myCanvas.create_oval(145, 150, 160, 165, fill="white") #right arm
+        #HIS right arm (FRONT ARM)
         a_Rarm = self.myCanvas.create_oval(145, 150, 160, 165, fill="white")
         
-        #HIS right leg
-        self.myCanvas.create_oval(145, 175, 160, 183, fill="saddle brown") #right arm
+        #HIS right leg (FRONT LEG)
         a_Rleg = self.myCanvas.create_oval(145, 175, 160, 183, fill="saddle brown")
 
-        #HIS left leg
-        self.myCanvas.create_oval(115, 175, 130, 183, fill="orange4") #right arm
-        a_Lleg = self.myCanvas.create_oval(115, 175, 130, 183, fill="orange4")
+        #HIS left leg (BACK LEG)
+        a_Lleg = self.myCanvas.create_oval(115, 175, 130, 183, fill="saddle brown")
 
         #Bandana
-        self.myCanvas.create_polygon(125, 140, 150, 140, 150, 150, fill="black") #Creates a polygon (triangle) main
-        a_bandana1 = self.myCanvas.create_polygon(125, 140, 150, 140, 150, 150, fill="black")
-        self.myCanvas.create_polygon(125, 140, 120, 142, 120, 145, fill="black") #Knot down
-        a_bandana2 = self.myCanvas.create_polygon(125, 140, 120, 142, 120, 145, fill="black")
-        self.myCanvas.create_polygon(125, 140, 120, 138, 120, 141, fill="black") #Knot up
-        a_bandana3 = self.myCanvas.create_polygon(125, 140, 120, 138, 120, 141, fill="black")
+        a_bandana1 = self.myCanvas.create_polygon(125, 140, 150, 140, 150, 150, fill="black") #Creates a polyon (triangle). Main part of banadana
+        a_bandana2 = self.myCanvas.create_polygon(125, 140, 120, 142, 120, 145, fill="black") #Knot down
+        a_bandana3 = self.myCanvas.create_polygon(125, 140, 120, 138, 120, 141, fill="black") #Knot up
 
         #V-neck
-        self.myCanvas.create_polygon(130, 150, 145, 155, 145, 175, 130, 175, fill="dark blue")
         a_vneck = self.myCanvas.create_polygon(130, 150, 145, 155, 145, 175, 130, 175, fill="dark blue")
         
         #Coat
-        self.myCanvas.create_polygon(130, 150, 129, 145, 142, 145, 135, 175, 115, 177, 130, 160, fill="gray14")
         a_coat = self.myCanvas.create_polygon(130, 150, 129, 145, 142, 145, 135, 175, 115, 177, 130, 160, fill="gray14")
+
+        #HIS left arm PLACED HERE BC NEEDED FOR ANIMATION (BACK ARM)
+        a_Larm = self.myCanvas.create_oval(115, 150, 130, 165, fill="white")
 
         #Animation of character running across screen
         for count in range(120):
-            increment = 10*count
+            increment = 10*count #Arms and legs out
             self.myCanvas.coords(a_head, 125+increment, 125, 150+increment, 150)
             self.myCanvas.coords(a_name, 75+increment, 100)
             self.myCanvas.coords(a_torso, 130+increment, 150, 145+increment, 175)
@@ -78,7 +69,25 @@ class MainFrame (Frame):
             self.myCanvas.coords(a_vneck, 130+increment, 150, 145+increment, 155, 145+increment, 175, 130+increment, 175)
             self.myCanvas.coords(a_coat, 130+increment, 150, 129+increment, 145, 142+increment, 145, 135+increment, 175, 115+increment, 177, 130+increment, 160)
             self.myCanvas.update()
-            sleep(0.01)
+            sleep(0.5)
+
+            increment2 = 10*count #Arms and legs in
+            self.myCanvas.coords(a_head, 125+increment2, 125+3, 150+increment2, 150+3)
+            self.myCanvas.coords(a_name, 75+increment2, 100)
+            self.myCanvas.coords(a_torso, 130+increment2, 150+3, 145+increment2, 175+3)
+            self.myCanvas.coords(a_Larm, 130+increment, 160, 145+increment, 175) #LEft arm here (back arm) THIS ARM IS ABOVE TORSO
+            self.myCanvas.coords(a_Rarm, 130+increment, 150, 145+increment, 175) #Arm hides behind torso
+            self.myCanvas.coords(a_Rleg, 145+increment2-10, 175, 160+increment2-10, 183)
+            self.myCanvas.coords(a_Lleg, 115+increment2+10, 175, 130+increment2+10, 183)
+            self.myCanvas.coords(a_bandana1, 125+increment2, 140+3, 150+increment2, 140+3, 150+increment2, 150+3)
+            self.myCanvas.coords(a_bandana2, 125+increment2, 140+3, 120+increment2, 142+3, 120+increment2, 145+3)
+            self.myCanvas.coords(a_bandana3, 125+increment2, 140+3, 120+increment2, 138+3, 120+increment2, 141+3)
+            self.myCanvas.coords(a_vneck, 130+increment2, 150+3, 145+increment2, 155+3, 145+increment2, 175+3, 130+increment2, 175+3)
+            self.myCanvas.coords(a_coat, 130+increment2, 150+3, 129+increment2, 145+3, 142+increment2, 145+3, 135+increment2, 175+3, 115+increment2, 177+3, 130+increment2, 160)
+            
+
+            self.myCanvas.update()
+            sleep(0.5)
         
 main_frame = MainFrame()
 main_frame.mainloop()
