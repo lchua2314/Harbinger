e65f04f15e24e7aa2aeb5f01bb57b4c0290389e8
lchua2314 <46981223+lchua2314@users.noreply.github.com>
2019-07-13 17:40:23 -0700

    Added crouching animation.

--- Harbinger_1_2.py
+++ Harbinger_1_2.py
@@ -114,35 +114,37 @@ class MainFrame (Frame):
                 self.__Frame = 0
 
             elif self.__Frame == 0 and self.__sprint == False and self.__crouch == True: #Moving left CROUCHING FRAME #1
-                self.__x -= 1 #Arms and legs out self.__x 
-                self.myCanvas.coords(a_head, 125+self.__x, 125+self.__y, 150+self.__x, 150+self.__y)
+                self.__x -= 0.5 #Arms and legs out self.__x 
+                self.myCanvas.coords(a_head, 145+self.__x-40, 135+self.__y, 170+self.__x-40, 160+self.__y) #(+20, +10) difference from walking
                 self.myCanvas.coords(a_name, 138+self.__x, 110+self.__y)
-                self.myCanvas.coords(a_torso, 130+self.__x, 150+self.__y, 145+self.__x, 150+self.__y, 145+self.__x, 175+self.__y, 130+self.__x, 175+self.__y)
-                self.myCanvas.coords(a_Larm, 115+self.__x, 150+self.__y, 130+self.__x, 165+self.__y)
-                self.myCanvas.coords(a_Rarm, 145+self.__x, 150+self.__y, 160+self.__x, 165+self.__y)
-                self.myCanvas.coords(a_Rleg, 145+self.__x, 175+self.__y, 160+self.__x, 183+self.__y)
-                self.myCanvas.coords(a_Lleg, 115+self.__x, 175+self.__y, 130+self.__x, 183+self.__y)
-                self.myCanvas.coords(a_bandana1, 125+self.__x+25, 140+self.__y, 150+self.__x-25, 140+self.__y, 150+self.__x-25, 150+self.__y)
-                self.myCanvas.coords(a_bandana2, 125+self.__x+25, 140+self.__y, 120+self.__x+35, 142+self.__y, 120+self.__x+35, 145+self.__y)
-                self.myCanvas.coords(a_bandana3, 125+self.__x+25, 140+self.__y, 120+self.__x+35, 138+self.__y, 120+self.__x+35, 141+self.__y)
-                self.myCanvas.coords(a_vneck, 130+self.__x, 155+self.__y, 145+self.__x, 150+self.__y, 145+self.__x, 175+self.__y, 130+self.__x, 175+self.__y)
-                self.myCanvas.coords(a_coat, 145+self.__x, 145+self.__y, 133+self.__x, 145+self.__y, 140+self.__x, 175+self.__y, 160+self.__x, 177+self.__y, 145+self.__x, 160+self.__y)
+                self.myCanvas.coords(a_torso, 140+self.__x-4, 150+self.__y, 150+self.__x-24, 160+self.__y, 125+self.__x+26, 170+self.__y, 115+self.__x+46, 160+self.__y) #(10, 0), (5,10), (-20,-5), (-15,-15)
+                self.myCanvas.coords(a_Larm, 130+self.__x+16, 160+self.__y, 145+self.__x-14, 175+self.__y) #LEft arm here (back arm) THIS ARM IS ABOVE TORSO (-30,0)
+                self.myCanvas.coords(a_Rarm, 130+self.__x+16, 160+self.__y, 145+self.__x-14, 175+self.__y) #Arm hides behind torso (+30,0)
+                self.myCanvas.coords(a_Rleg, 145+self.__x-5, 175+self.__y, 160+self.__x-5, 183+self.__y) #(+5,-5), (+5,-5)
+                self.myCanvas.coords(a_Lleg, 115+self.__x+5, 175+self.__y, 130+self.__x+5, 183+self.__y) #(-5,-5), (-5,-5)
+                self.myCanvas.coords(a_bandana1, 145+self.__x-14, 150+self.__y, 170+self.__x-64, 150+self.__y, 170+self.__x-64, 160+self.__y) #(+20,10) for all bandana
+                self.myCanvas.coords(a_bandana2, 145+self.__x-14, 150+self.__y, 140+self.__x-4, 152+self.__y, 140+self.__x-4, 160+self.__y)
+                self.myCanvas.coords(a_bandana3, 145+self.__x-14, 150+self.__y, 140+self.__x-4, 148+self.__y, 140+self.__x-4, 151+self.__y) 
+                self.myCanvas.coords(a_vneck, 140+self.__x-4, 150+self.__y, 145+self.__x-14, 163+self.__y, 125+self.__x+26, 170+self.__y, 115+self.__x+46, 160+self.__y) #(10,0), (0,8), (-20,-5), (-15,-15)
+                self.myCanvas.coords(a_coat, 142+self.__x-8, 145+self.__y, 152+self.__x-28, 155+self.__y, 85+self.__x+76, 180+self.__y, 80+self.__x+104, 180+self.__y, 125+self.__x+26, 155+self.__y)
                 self.__Frame = 1
                 
             elif self.__Frame == 1 and self.__sprint == False and self.__crouch == True: #Moving left CROUCHING FRAME #2
-                self.__x -= 1 #Arms and legs in
-                self.myCanvas.coords(a_head, 125+self.__x, 125+3+self.__y, 150+self.__x, 150+3+self.__y)
+                self.__x -= 0.5 #Arms and legs in
+                self.myCanvas.coords(a_head, 145+self.__x-40, 135+2+self.__y, 170+self.__x-40, 160+2+self.__y) #(+20, +10) difference from walking
                 self.myCanvas.coords(a_name, 138+self.__x, 110+self.__y)
-                self.myCanvas.coords(a_torso, 130+self.__x, 150+3+self.__y, 145+self.__x, 150+3+self.__y, 145+self.__x, 175+3+self.__y, 130+self.__x, 175+3+self.__y)
-                self.myCanvas.coords(a_Larm, 130+self.__x, 160+self.__y, 145+self.__x, 175+self.__y) #LEft arm here (back arm) THIS ARM IS ABOVE TORSO
-                self.myCanvas.coords(a_Rarm, 130+self.__x, 160+self.__y, 145+self.__x, 175+self.__y) #Arm hides behind torso
+                self.myCanvas.coords(a_torso, 140+self.__x-4, 150+2+self.__y, 150+self.__x-24, 160+2+self.__y, 125+self.__x+26, 170+2+self.__y, 115+self.__x+46, 160+2+self.__y) #(20, 10), (5,10)
+                self.myCanvas.coords(a_Larm, 130+self.__x-10, 160+2+self.__y, 145+self.__x-10, 175+2+self.__y) #LEft arm here (back arm) THIS ARM IS ABOVE TORSO (-30,0)
+                self.myCanvas.coords(a_Rarm, 130+self.__x-10, 160+2+self.__y, 145+self.__x-10, 175+2+self.__y) #Arm hides behind torso (+30,0)
                 self.myCanvas.coords(a_Rleg, 145+self.__x-10, 175+self.__y, 160+self.__x-10, 183+self.__y)
                 self.myCanvas.coords(a_Lleg, 115+self.__x+10, 175+self.__y, 130+self.__x+10, 183+self.__y)
-                self.myCanvas.coords(a_bandana1, 125+self.__x+25, 140+3+self.__y, 150+self.__x-25, 140+3+self.__y, 150+self.__x-25, 150+3+self.__y)
-                self.myCanvas.coords(a_bandana2, 125+self.__x+25, 140+3+self.__y, 120+self.__x+35, 142+3+self.__y, 120+self.__x+35, 145+3+self.__y)
-                self.myCanvas.coords(a_bandana3, 125+self.__x+25, 140+3+self.__y, 120+self.__x+35, 138+3+self.__y, 120+self.__x+35, 141+3+self.__y)
-                self.myCanvas.coords(a_vneck, 130+self.__x, 155+3+self.__y, 145+self.__x, 150+3+self.__y, 145+self.__x, 175+3+self.__y, 130+self.__x, 175+3+self.__y)
-                self.myCanvas.coords(a_coat, 145+self.__x, 145+3+self.__y, 133+self.__x, 145+3+self.__y, 140+self.__x, 175+3+self.__y, 160+self.__x, 177+6+self.__y, 145+self.__x, 160+3+self.__y)
+                self.myCanvas.coords(a_bandana1, 145+self.__x-14, 150+2+self.__y, 170+self.__x-64, 150+2+self.__y, 170+self.__x-64, 160+2+self.__y) #(+20,10) for all bandana
+                self.myCanvas.coords(a_bandana2, 145+self.__x-14, 150+2+self.__y, 140+self.__x-4, 152+2+self.__y, 140+self.__x-4, 160+2+self.__y)
+                self.myCanvas.coords(a_bandana3, 145+self.__x-14, 150+2+self.__y, 140+self.__x-4, 148+2+self.__y, 140+self.__x-4, 151+2+self.__y) 
+                self.myCanvas.coords(a_vneck, 140+self.__x-4, 150+2+self.__y, 145+self.__x-14, 163+2+self.__y, 125+self.__x+26, 170+2+self.__y, 115+self.__x+46, 160+2+self.__y) #(10,0), (0,8), (-20,-5), (-15,-15)
+                self.myCanvas.coords(a_coat, 142+self.__x-8, 145+2+self.__y, 152+self.__x-28, 155+2+self.__y, 85+self.__x+76, 180+2+self.__y, 80+self.__x+104, 180+2+self.__y, 125+self.__x+26, 155+2+self.__y)
+                #(12,0) (10,10), (-35,0), (-30,-20), (-5,-5)
+
                 self.__Frame = 0
                 
             self.myCanvas.update()
@@ -205,7 +207,7 @@ class MainFrame (Frame):
                 self.myCanvas.coords(a_name, 138+self.__x, 110+self.__y)
                 self.myCanvas.coords(a_torso, 140+self.__x, 150+5+self.__y, 150+self.__x, 160+5+self.__y, 125+self.__x, 170+5+self.__y, 115+self.__x, 160+5+self.__y) #(20, 10), (5,10)
                 self.myCanvas.coords(a_Larm, 130+self.__x, 160+self.__y, 145+self.__x, 175+self.__y) #LEft arm here (back arm) THIS ARM IS ABOVE TORSO (-30,0)
-                self.myCanvas.coords(a_Rarm, 130+self.__x, 150+self.__y, 145+self.__x, 175+self.__y) #Arm hides behind torso (+30,0)
+                self.myCanvas.coords(a_Rarm, 130+self.__x, 160+self.__y, 145+self.__x, 175+self.__y) #Arm hides behind torso (+30,0)
                 self.myCanvas.coords(a_Rleg, 145+self.__x-10, 175+self.__y, 160+self.__x-10, 183+self.__y)
                 self.myCanvas.coords(a_Lleg, 115+self.__x+10, 175+self.__y, 130+self.__x+10, 183+self.__y)
                 self.myCanvas.coords(a_bandana1, 145+self.__x, 150+5+self.__y, 170+self.__x, 150+5+self.__y, 170+self.__x, 160+5+self.__y) #(+20,10) for all bandana
@@ -218,35 +220,34 @@ class MainFrame (Frame):
                 self.__Frame = 0
 
             elif self.__Frame == 0 and self.__sprint == False and self.__crouch == True: #Moving right CROUCHING FRAMe #1
-                self.__x += 1 #Arms and legs out self.__x 
-                self.myCanvas.coords(a_head, 125+self.__x, 125+self.__y, 150+self.__x, 150+self.__y)
+                self.myCanvas.coords(a_head, 145+self.__x, 135+self.__y, 170+self.__x, 160+self.__y) #(+20, +10) difference from walking
                 self.myCanvas.coords(a_name, 138+self.__x, 110+self.__y)
-                self.myCanvas.coords(a_torso, 130+self.__x, 150+self.__y, 145+self.__x, 150+self.__y, 145+self.__x, 175+self.__y, 130+self.__x, 175+self.__y)
-                self.myCanvas.coords(a_Larm, 115+self.__x, 150+self.__y, 130+self.__x, 165+self.__y)
-                self.myCanvas.coords(a_Rarm, 145+self.__x, 150+self.__y, 160+self.__x, 165+self.__y)
-                self.myCanvas.coords(a_Rleg, 145+self.__x, 175+self.__y, 160+self.__x, 183+self.__y)
-                self.myCanvas.coords(a_Lleg, 115+self.__x, 175+self.__y, 130+self.__x, 183+self.__y)
-                self.myCanvas.coords(a_bandana1, 125+self.__x, 140+self.__y, 150+self.__x, 140+self.__y, 150+self.__x, 150+self.__y)
-                self.myCanvas.coords(a_bandana2, 125+self.__x, 140+self.__y, 120+self.__x, 142+self.__y, 120+self.__x, 145+self.__y)
-                self.myCanvas.coords(a_bandana3, 125+self.__x, 140+self.__y, 120+self.__x, 138+self.__y, 120+self.__x, 141+self.__y)
-                self.myCanvas.coords(a_vneck, 130+self.__x, 150+self.__y, 145+self.__x, 155+self.__y, 145+self.__x, 175+self.__y, 130+self.__x, 175+self.__y)
-                self.myCanvas.coords(a_coat, 130+self.__x, 145+self.__y, 142+self.__x, 145+self.__y, 135+self.__x, 175+self.__y, 115+self.__x, 177+self.__y, 130+self.__x, 160+self.__y)
+                self.myCanvas.coords(a_torso, 140+self.__x, 150+self.__y, 150+self.__x, 160+self.__y, 125+self.__x, 170+self.__y, 115+self.__x, 160+self.__y) #(10, 0), (5,10), (-20,-5), (-15,-15)
+                self.myCanvas.coords(a_Larm, 130+self.__x, 160+self.__y, 145+self.__x, 175+self.__y) #LEft arm here (back arm) THIS ARM IS ABOVE TORSO (-30,0)
+                self.myCanvas.coords(a_Rarm, 130+self.__x, 160+self.__y, 145+self.__x, 175+self.__y) #Arm hides behind torso (+30,0)
+                self.myCanvas.coords(a_Rleg, 145+self.__x-5, 175+self.__y, 160+self.__x-5, 183+self.__y) #(+5,-5), (+5,-5)
+                self.myCanvas.coords(a_Lleg, 115+self.__x+5, 175+self.__y, 130+self.__x+5, 183+self.__y) #(-5,-5), (-5,-5)
+                self.myCanvas.coords(a_bandana1, 145+self.__x, 150+self.__y, 170+self.__x, 150+self.__y, 170+self.__x, 160+self.__y) #(+20,10) for all bandana
+                self.myCanvas.coords(a_bandana2, 145+self.__x, 150+self.__y, 140+self.__x, 152+self.__y, 140+self.__x, 160+self.__y)
+                self.myCanvas.coords(a_bandana3, 145+self.__x, 150+self.__y, 140+self.__x, 148+self.__y, 140+self.__x, 151+self.__y) 
+                self.myCanvas.coords(a_vneck, 140+self.__x, 150+self.__y, 145+self.__x, 163+self.__y, 125+self.__x, 170+self.__y, 115+self.__x, 160+self.__y) #(10,0), (0,8), (-20,-5), (-15,-15)
+                self.myCanvas.coords(a_coat, 142+self.__x, 145+self.__y, 152+self.__x, 155+self.__y, 115+self.__x, 180+self.__y, 92+self.__x, 180+self.__y, 125+self.__x, 155+self.__y)
                 self.__Frame = 1
                 
             elif self.__Frame == 1 and self.__sprint == False and self.__crouch == True: #Moving right CROUCHING FRAMe #2
-                self.__x += 1 #Arms and legs in
-                self.myCanvas.coords(a_head, 125+self.__x, 125+3+self.__y, 150+self.__x, 150+3+self.__y)
+                self.__x += 0.5 #Arms and legs in
+                self.myCanvas.coords(a_head, 145+self.__x, 135+2+self.__y, 170+self.__x, 160+2+self.__y) #(+20, +10) difference from walking
                 self.myCanvas.coords(a_name, 138+self.__x, 110+self.__y)
-                self.myCanvas.coords(a_torso, 130+self.__x, 150+3+self.__y, 145+self.__x, 150+3+self.__y, 145+self.__x, 175+3+self.__y, 130+self.__x, 175+3+self.__y)
-                self.myCanvas.coords(a_Larm, 130+self.__x, 160+self.__y, 145+self.__x, 175+self.__y) #LEft arm here (back arm) THIS ARM IS ABOVE TORSO
-                self.myCanvas.coords(a_Rarm, 130+self.__x, 160+self.__y, 145+self.__x, 175+self.__y) #Arm hides behind torso
+                self.myCanvas.coords(a_torso, 140+self.__x, 150+2+self.__y, 150+self.__x, 160+2+self.__y, 125+self.__x, 170+2+self.__y, 115+self.__x, 160+2+self.__y) #(20, 10), (5,10)
+                self.myCanvas.coords(a_Larm, 130+self.__x+10, 160+2+self.__y, 145+self.__x+10, 175+2+self.__y) #LEft arm here (back arm) THIS ARM IS ABOVE TORSO (-30,0)
+                self.myCanvas.coords(a_Rarm, 130+self.__x+10, 160+2+self.__y, 145+self.__x+10, 175+2+self.__y) #Arm hides behind torso (+30,0)
                 self.myCanvas.coords(a_Rleg, 145+self.__x-10, 175+self.__y, 160+self.__x-10, 183+self.__y)
                 self.myCanvas.coords(a_Lleg, 115+self.__x+10, 175+self.__y, 130+self.__x+10, 183+self.__y)
-                self.myCanvas.coords(a_bandana1, 125+self.__x, 140+3+self.__y, 150+self.__x, 140+3+self.__y, 150+self.__x, 150+3+self.__y)
-                self.myCanvas.coords(a_bandana2, 125+self.__x, 140+3+self.__y, 120+self.__x, 142+3+self.__y, 120+self.__x, 145+3+self.__y)
-                self.myCanvas.coords(a_bandana3, 125+self.__x, 140+3+self.__y, 120+self.__x, 138+3+self.__y, 120+self.__x, 141+3+self.__y)
-                self.myCanvas.coords(a_vneck, 130+self.__x, 150+3+self.__y, 145+self.__x, 155+3+self.__y, 145+self.__x, 175+3+self.__y, 130+self.__x, 175+3+self.__y)
-                self.myCanvas.coords(a_coat, 130+self.__x, 145+3+self.__y, 142+self.__x, 145+3+self.__y, 135+self.__x, 175+3+self.__y, 115+self.__x, 177+6+self.__y, 130+self.__x, 160+self.__y)
+                self.myCanvas.coords(a_bandana1, 145+self.__x, 150+2+self.__y, 170+self.__x, 150+2+self.__y, 170+self.__x, 160+2+self.__y) #(+20,10) for all bandana
+                self.myCanvas.coords(a_bandana2, 145+self.__x, 150+2+self.__y, 140+self.__x, 152+2+self.__y, 140+self.__x, 155+2+self.__y)
+                self.myCanvas.coords(a_bandana3, 145+self.__x, 150+2+self.__y, 140+self.__x, 148+2+self.__y, 140+self.__x, 151+2+self.__y)
+                self.myCanvas.coords(a_vneck, 140+self.__x, 150+2+self.__y, 145+self.__x, 163+2+self.__y, 125+self.__x, 170+2+self.__y, 115+self.__x, 160+2+self.__y) #(10,0), (0,8), (-20,-5), (-15,-15)
+                self.myCanvas.coords(a_coat, 142+self.__x, 145+2+self.__y, 152+self.__x, 155+2+self.__y, 115+self.__x, 180+2+self.__y, 92+self.__x, 180+2+self.__y, 125+self.__x, 155+2+self.__y)
                 self.__Frame = 0
                 
             self.myCanvas.update()
@@ -283,22 +284,23 @@ class MainFrame (Frame):
                 y2=y1+random.randrange(1000)
                 self.myCanvas.create_rectangle(x1,y1,x2,y2,outline="black")
 
-        self.master.title("Harbinger - Alpha 1.3: Keybinding Update") #Titles the window "Harbinger"
+        self.master.title("Harbinger - Alpha 1.5: Crouching Update") #Titles the window "Harbinger"
 
         self.myCanvas = Canvas(width=1500, height=1000, bg="SkyBlue1") #Background with window size. bg= "SkyBlue1"
         self.myCanvas.grid() #Display it
 
         #randomRects(300) #BACKGROUND
         
-        a_description = self.myCanvas.create_text(0, 0, text="""Press 'd' or 'a' to move. Hold 'd' or 'a' to walk. Press Shift to change from walking to running. Vise-vera."""
-                                                  """ Press left and right mouse buttons do nothing but show up on the interpeter."""
-                                                  """ Will try to make him jump and crouch next update""",
+        a_description = self.myCanvas.create_text(0, 0, text="""Press 'd' or 'a' to move. Hold 'd' or 'a' to walk. \nPress Shift to change from walking to running. Vise-vera."""
+                                                  """ \nPress Ctrl to toggle crouching."""
+                                                  """ \nActions are displayed on the interpreter."""
+                                                  """ \nWill try to make him jump next update.""",
                                                   width=1000, fill="black", anchor="nw", font=("Fixedsys", 16))#Top left corner of screen text
         
         #Creating the model of player 1 w/ label above it's head
         #Character is walking to the right of the screen
         #Name of the character
-        a_name = self.myCanvas.create_text(138, 110, text="Player", width=1000, fill="green", anchor="center", font=("Fixedsys", 16))
+        a_name = self.myCanvas.create_text(138, 110, text="Titus", width=1000, fill="green", anchor="center", font=("Fixedsys", 16))
 
         #Creates the head of character
         a_head = self.myCanvas.create_oval(125, 125, 150, 150, fill="white")
@@ -307,7 +309,7 @@ class MainFrame (Frame):
         a_Rarm = self.myCanvas.create_oval(130, 160, 145, 175, fill="white")
         
         #Torso
-        a_torso = self.myCanvas.create_polygon(130, 150, 145, 150, 145, 175, 130, 175, fill="white")
+        a_torso = self.myCanvas.create_polygon(130, 150, 145, 150, 145, 175, 130, 175, fill="white", outline="black")
 
         #HIS right leg (FRONT LEG)
         a_Rleg = self.myCanvas.create_oval(135, 175, 150, 183, fill="saddle brown")
@@ -317,6 +319,8 @@ class MainFrame (Frame):
 
         #Bandana
         a_bandana1 = self.myCanvas.create_polygon(125, 140, 150, 140, 150, 150, fill="black") #Creates a polyon (triangle). Main part of banadana
+        a_bandana2 = self.myCanvas.create_polygon(125, 140, 120, 142, 120, 145, fill="black") #Knot down
+        a_bandana3 = self.myCanvas.create_polygon(125, 140, 120, 138, 120, 141, fill="black") #Knot up
 
         #V-neck
         a_vneck = self.myCanvas.create_polygon(130, 150, 145, 155, 145, 175, 130, 175, fill="dark blue")
@@ -326,10 +330,6 @@ class MainFrame (Frame):
 
         #HIS left arm PLACED HERE BC NEEDED FOR ANIMATION (BACK ARM)
         a_Larm = self.myCanvas.create_oval(130, 160, 145, 175, fill="white")
-
-        #Other bandanna parts moved here so they overlap the coat
-        a_bandana2 = self.myCanvas.create_polygon(125, 140, 120, 142, 120, 145, fill="black") #Knot down
-        a_bandana3 = self.myCanvas.create_polygon(125, 140, 120, 138, 120, 141, fill="black") #Knot up
 
         #KEYBINDINGS
         self.myCanvas.bind("<Button-1>", leftClick) #bind.("<BUTTON_NAME>", function_call) #Functions way above
