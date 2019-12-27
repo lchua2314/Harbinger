e18722930e68ed7f3c0f0c2adedcf2c1b3016979
Lawrence Chua <lchua2314@gmail.com>
2019-12-26 17:51:01 -0800

    Removed all "unrelated" files.

--- NEW 9 Canvas Arcs.py
+++
@@ -1,9 +0,0 @@
-from tkinter import*
-import random
-root=Tk()
-canvas=Canvas(root,width=300,height=300)
-canvas.pack()
-canvas.create_arc(10,10,200,80,extent=45,style=ARC)
-canvas.create_arc(10,80,200,160,extent=90,style=ARC)
-
-root.mainloop()
